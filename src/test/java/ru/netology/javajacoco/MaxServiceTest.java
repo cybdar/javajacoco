a26ef88becd5644156df6ca4503f562d9c146bf2
cybdar <kari4ka21@gmail.com>
2025-10-17 15:57:10 +0200

    Add MaxService and tests

---
+++ src/test/java/ru/netology/javajacoco/MaxServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.javajacoco;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MaxServiceTest {
+
+    @Test
+    public void shouldFindMaxIfA() {
+        MaxService service = new MaxService();
+
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxIfB() {
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
